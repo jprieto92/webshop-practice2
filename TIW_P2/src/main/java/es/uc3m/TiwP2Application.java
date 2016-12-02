773b36ab3e0bc24b611cb73417fc58fbb5c5f4f1
Álvaro P. <100303631@alumnos.uc3m.es>
2016-12-02 17:06:24 +0000

    Primer commit del proyecto

---
+++ TIW_P2/src/main/java/es/uc3m/TiwP2Application.java
@@ -0,0 +1,12 @@
+package es.uc3m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiwP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiwP2Application.class, args);
+	}
+}
