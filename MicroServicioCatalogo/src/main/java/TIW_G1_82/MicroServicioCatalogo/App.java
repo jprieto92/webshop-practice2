e14b7541e2e1fb57f62e521ea0d550950f6ac335
JesusCI <elbu93@gmail.com>
2016-12-05 17:58:14 +0000

    Creacion de los microservicios

---
+++ MicroServicioCatalogo/src/main/java/TIW_G1_82/MicroServicioCatalogo/App.java
@@ -0,0 +1,13 @@
+package TIW_G1_82.MicroServicioCatalogo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
