ceee484dd70e41507dcf4e1baa1e2913b2265e22
Álvaro P. <100303631@alumnos.uc3m.es>
2016-12-05 19:25:04 +0000

    Corregidos proyectos microservicios.

---
+++ Usuario/src/main/java/es/uc3m/UsuarioApplication.java
@@ -0,0 +1,12 @@
+package es.uc3m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioApplication.class, args);
+	}
+}
